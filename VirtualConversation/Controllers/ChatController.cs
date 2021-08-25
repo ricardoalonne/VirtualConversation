84f9f0e99ab44aa3b3621572a1d657340aff6ff6
ricardoalonne <71679195+ricardoalonne@users.noreply.github.com>
2021-08-25 10:25:09 -0500

    Agregar archivos de proyecto.

---
+++ VirtualConversation/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualConversation.Controllers
+{
+    public class ChatController : Controller
+    {
+
+        public static Dictionary<int, string> Rooms = new Dictionary<int, string>()
+        {
+            {1,"Sala General"},
+            {2,"Sala De Estudios 01"},
+            {3,"Sala De Proyecciones 01"}
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int idRoom)
+        {
+            return View("Room", idRoom);
+        }
+    }
+}
---
+++ VirtualConversation/Logic/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualConversation.Logic
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            await Clients.Group(room).SendAsync("MessageReceived", user, message);
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("ShowWho", $"Alguien está en línea ({Context.ConnectionId})");
+        }
+
+    }
+}
